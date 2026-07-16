8f9d52d3c243da2dbe146caec39a0d5989a6065f
Prian Senthilkumar <priansenthilkumar99@gmail.com>
2026-07-16 10:57:06 +0530

    Change date to dark grey #555e6b for visibility on white token

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function buildToken(n, total) {
     + verifyBadge
     + '</div>'
     + '<div class="tok-meal-row" style="background:' + bg + ';color:' + acc + ';">' + mealLabel + '</div>'
-    + '<div class="tok-date" style="color:#b0b8c1;font-weight:700;font-size:.60rem;">' + getTodayDate() + '</div>'
+    + '<div class="tok-date" style="color:#555e6b;font-weight:700;font-size:.60rem;">' + getTodayDate() + '</div>'
     + '</div>'
     + '</div>';
 }
